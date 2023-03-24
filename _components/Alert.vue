5b401204f2e610b6e223acff98702abe8cd74d9a
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-03-24 11:24:14 -0500

    delete banner

--- _components/Alert.vue
+++ _components/Alert.vue
@@ -1,7 +1,4 @@
 <template>
-  <q-banner v-if="true" inline-actions class="text-black bg-warning text-center offline-bar">
-    {{ $tr('isite.cms.message.appOffline') }}
-  </q-banner>
 </template>
 
 <script>
@@ -20,26 +17,21 @@ export default {
     connectionSwitch() {
       if (navigator.onLine) {
         this.$store.dispatch("qofflineMaster/APP_ONLINE")
-      }else{
+      } else {
         this.$store.dispatch("qofflineMaster/APP_OFFLINE")
+        this.$q.notify({
+          color: 'warning',
+          icon: 'fa-light fa-cloud-slash',
+          message: this.$tr('isite.cms.message.appOffline'),
+          position: 'bottom-left',
+          timeout: 200
+        })
       }
+      
     },
   }
 }
 </script>
 
-<style scoped>
-  .offline-bar{
-    position: fixed;
-    bottom: 60px;
-    width: 100%;
-    transform: translateX(-15px);
-    min-height: 27px !important;
-  }
-  @media(min-width: 991px){
-      .offline-bar{
-        bottom: 0;
-        transform: translateX(-20px);
-      }
-    }
+<style>
 </style>
