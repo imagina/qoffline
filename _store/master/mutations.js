3cf2a4e092e29a4b11089872c6876e257624a0a0
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-11-14 10:18:52 -0500

    Fix: validating that "data" exists

--- _store/master/mutations.js
+++ _store/master/mutations.js
@@ -5,13 +5,13 @@ export const HANDLE_MODAL_SYNC = (state, data) => {
   state.isOpenModalSync = data;
 };
 
-export function SET_REQUESTS (state, data) {
+export function SET_REQUESTS (state, data=[]) {
   const STATUS = 'PENDING'
   
   state.requestsReversed = data?.reverse()
 
   state.totalRequests = data?.length
-  const pendingRequests = data.filter(request => request.metadata.status === STATUS)
+  const pendingRequests = data?.filter(request => request.metadata.status === STATUS)
   state.pendingRequests = pendingRequests.length
 }
 
