dd0a4d5021cd6a389d2dbbc4809b6c75784f3057
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-11-14 10:30:35 -0500

    Refactor SET_REQUESTS mutation to handle data in null

--- _store/master/mutations.js
+++ _store/master/mutations.js
@@ -8,11 +8,11 @@ export const HANDLE_MODAL_SYNC = (state, data) => {
 export function SET_REQUESTS (state, data=[]) {
   const STATUS = 'PENDING'
   
-  state.requestsReversed = data?.reverse()
+  state.requestsReversed = data?.reverse() || []
 
-  state.totalRequests = data?.length
+  state.totalRequests = data?.length || 0
   const pendingRequests = data?.filter(request => request.metadata.status === STATUS)
-  state.pendingRequests = pendingRequests.length
+  state.pendingRequests = pendingRequests?.length || 0
 }
 
 export const APP_ONLINE = (state) => {
